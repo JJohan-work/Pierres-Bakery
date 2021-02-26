c161265866ca9d90fc83bf4f5f1d9910e9f09d06
Jonah Johansen <johansenjonah+git@gmail.com>
2021-02-26 15:06:04 -0800

    add first working version of UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -23,21 +23,12 @@ namespace Bakery
       
       if (response.ToLower() == "bread")
       {
-        Bread breadItem = new Bread();
-        Console.Write("How many loafs would you like to buy?: ");
-        int amountToAdd = Int32.Parse(Console.ReadLine());
-        breadItem.AddAmount(amountToAdd);
-        breadCart.Add(breadItem);
-        InitialPrompt();
+        OrderBread();
       }
-      // else if (response.ToLower() == "pastry")
-      // {
-      //   Pastry pastryCart = new Pastry();
-      //   Console.Write("How many pastries would you like to buy?: ");
-      //   int amountToAdd = Int32.Parse(Console.ReadLine());
-      //   pastryCart.AddAmount(amountToAdd);
-      //   InitialPrompt();
-      // }
+      if (response.ToLower() == "pastry")
+      {
+        OrderPastry();
+      }
       else if (response.ToLower() == "checkout")
       {
         if (breadCart.Count == 0 && pastryCart.Count == 0)
@@ -47,12 +38,32 @@ namespace Bakery
         }
         else
         {
-          foreach (Bread item in breadCart)
+          int totalBreadCost = 0;
+          int totalPastryCost = 0;
+
+          if (breadCart.Count != 0)
           {
-            Console.WriteLine($"{item.GetCost().ToString()}");
+            Console.WriteLine("Bread_________________");
+            foreach (Bread item in breadCart)
+            {
+              Console.WriteLine($"    Type:{item.GetBakeType()} Amount:{item.GetAmount()} Cost:{item.GetCost().ToString()}");
+              totalBreadCost += item.GetCost();
+            }
+            Console.WriteLine($"    Bread Total: {totalBreadCost.ToString()}");
           }
 
-          // Console.WriteLine($"Your Total is: ${breadCart.GetCost() + pastryCart.GetCost()}");
+          if (pastryCart.Count != 0)
+          {
+            Console.WriteLine("Pastries_________________");
+            foreach (Pastry item in pastryCart)
+            {
+              Console.WriteLine($"    Type:{item.GetBakeType()} Amount:{item.GetAmount()} Cost:{item.GetCost().ToString()}");
+              totalPastryCost += item.GetCost();
+            }
+            Console.WriteLine($"    Pastry Total: {totalPastryCost.ToString()}");
+          }
+
+          Console.WriteLine($"Total Order Cost: {(totalBreadCost + totalPastryCost).ToString()}");
           Console.WriteLine("Thank you for Shopping at Pierre's Bakery! Have a good day!");
         }
       }
@@ -60,6 +71,36 @@ namespace Bakery
       {
         Console.WriteLine("GoodBye!");
       }
+    }
+    public static void OrderPastry()
+    {
+      Pastry pastryItem = new Pastry();
+      Console.Write("How many pastries would you like to buy?: ");
+      int amountToAdd = Int32.Parse(Console.ReadLine());
+
+      Console.WriteLine("Types of Pastries:");
+      Console.WriteLine("   Flaky\n   Puff\n   ShortCrust\n   Cinnamon rolls\n");
+
+      Console.Write("What Type of pastrie would you like to buy? (flaky/puff/short/cinnamon): ");
+      string type = Console.ReadLine();
+      pastryItem.AddAmount(amountToAdd);
+      pastryItem.SetBakeType(type);
+      pastryCart.Add(pastryItem);
+      InitialPrompt();
+    }
+    public static void OrderBread()
+    {
+      Bread breadItem = new Bread();
+      Console.Write("How many loafs would you like to buy?: ");
+      int amountToAdd = Int32.Parse(Console.ReadLine());
+      Console.WriteLine("Types of Bread:");
+      Console.WriteLine("   Plain\n   Garlic\n   Sourdough\n   Herbs\n");
+      Console.Write("What Type of bread would you like to buy? (plain/garlic/sour/herb): ");
+      string type = Console.ReadLine();
+      breadItem.AddAmount(amountToAdd);
+      breadItem.SetBakeType(type);
+      breadCart.Add(breadItem);
+      InitialPrompt();
     }
   }
 }
